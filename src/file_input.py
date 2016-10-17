3a00cac8f36ebec8f49da862a94183e247dec5d0
Fredrik Johansson <f95johansson@gmail.com>
2016-10-17 14:39:26 +0200

    Added extra documentation

--- src/file_input.py
+++ src/file_input.py
@@ -9,6 +9,8 @@ def open_file(path, ordered=False):
     """
     Open a text file and evaluates the data according to the syntax specified in
     https://www8.cs.umu.se/kurser/5DV121/HT15/assignment2/faces.html
+    :param path: String path to file
+    :return: dict with key: <id> and value: <image>
     """
 
     data = {} if not ordered else OrderedDict()
@@ -17,7 +19,7 @@ def open_file(path, ordered=False):
             iterator = iter(file)
 
             while True:
-                line = iterator.__next__().rstrip()
+                line = next(iterator).rstrip()
                 if line.startswith('#'): pass
                 elif line == '': pass
                 elif line.startswith('Image'):
@@ -29,6 +31,7 @@ def open_file(path, ordered=False):
                         image = _read_image(iterator)
                         image = pre.rotate(image)
                         image = pre.normalize(image)
+                        image.append(1) # bias
                         data[id] = image
                 else:
                     raise InvalidFileError("With line: "+line)
@@ -40,22 +43,32 @@ def open_file(path, ordered=False):
     return data
 
 def _read_answer(line):
-    """Ger id and answer from a line with format "Image<ID> <Answer>" """
+    """
+    Ger id and answer from a line with format "Image<ID> <Answer>"
+    :param line: line, string with assumed answer and id
+    :return: id, answer
+    """
     id_string, answer_string = line.split(' ')
     id = int(id_string[len('Image'):])
     answer = int(answer_string)
     return id, answer
 
 def _read_id(line):
-    """Get id from a line with format "Image<ID" """
+    """
+    Get id from a line with format "Image<ID>"
+    :param line: line, string with assumed id
+    :return: id
+    """
     return int(line[len('Image'):])
 
 def _read_image(iterator):
     """
     Get the image from the next IMAGE_SIZE lines
     Returns a list of the pixel values
+    :param iterator: iterator for file
+    :return: image in form of lists of ints
     """
     image = []
     for i in range(IMAGE_SIZE):
-        image.extend(list(map(int, iterator.__next__().split(' '))))
+        image.extend(list(map(int, next(iterator).split(' '))))
     return image
--- src/neural_network.py
+++ src/neural_network.py
@@ -6,8 +6,8 @@ LEARNING_RATE = 0.01
 
 class NeuralNetwork:
     """
-        Keeps track of the perceptron. Controls when the perceptron are
-        training and when to test.
+    Keeps track of the perceptron. Controls when the perceptron are
+    training and when to test.
     """
     def __init__(self, training_set, facit_set):
         self._training_set = training_set
@@ -22,9 +22,9 @@ class NeuralNetwork:
 
     def learn(self):
         """
-            This method is the part where the program learns the perceptron to
-            recognize moods in pictures. It keeps track of the total error in
-            order to quit the learningface when the error is small enough.
+        This method is the part where the program learns the perceptron to
+        recognize moods in pictures. It keeps track of the total error in
+        order to quit the learningface when the error is small enough.
         """
         total_error = 0
         threshold = 0.05
@@ -38,8 +38,8 @@ class NeuralNetwork:
 
     def check(self, test_set):
         """
-            This is the method used when the image answer is not available for
-            the program to read.  
+        This is the method used when the image answer is not available for
+        the program to read.  
         """
         for id in test_set.keys():
             image = test_set[id]
@@ -51,17 +51,17 @@ class NeuralNetwork:
 
     def learning_step(self):
         """
-            This is where the actual trining is happening. The method sends an
-            image with it's answer to the perceptron. One training session is
-            one lap with a specific set of picture. It also returns the total
-            error of that session.
+        This is where the actual trining is happening. The method sends an
+        image with it's answer to the perceptron. One training session is
+        one lap with a specific set of picture. It also returns the total
+        error of that session.
         """
-         ids = list(self._training_set.keys())
-         shuffle(ids)
-         total_error=0
-         for id in ids:
-             image = self._training_set[id]
-             for mood, perceptron in self._perceptrons.items():
-                 answer = int(mood.value == self._facit_set[id])
-                 total_error += perceptron.train(image, answer)**2
-         return total_error
+        ids = list(self._training_set.keys())
+        shuffle(ids)
+        total_error=0
+        for id in ids:
+            image = self._training_set[id]
+            for mood, perceptron in self._perceptrons.items():
+                answer = int(mood.value == self._facit_set[id])
+                total_error += perceptron.train(image, answer)**2
+        return total_error
