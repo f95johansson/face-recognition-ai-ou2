f972877b3c2b6adca5a1845fb96d0a53ecf1de8b
Fredrik Johansson <f95johansson@gmail.com>
2016-10-18 13:06:20 +0200

    Added documentation

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -7,18 +7,30 @@ IMAGE_SIZE = 20
 HIGHEST_PIXEL_VALUE = 31
 
 def normalize(image):
-    """Returns image with pixel values between 0-1"""
+    """
+    Returns image with pixel values between 0-1
+    :param image: in form of a list with values from 0 to 31
+    :return: list with values form 0 to 1
+    """
     return [x / HIGHEST_PIXEL_VALUE for x in image]
 
 def highpass_filter(image):
-    """Perform highpass filter on a non-normalized image"""
+    """
+    Perform highpass filter on a non-normalized image
+    :param image: in form of a list with values from 0 to 31
+    :return: list with values with lower values under 8 set to 0
+    """
     for i,value in enumerate(image):
         if value <= 8:
             image[i] = 0
     return image
 
 def contrast(image_list):
-    """Increases the image contrast on a non-normalized image"""
+    """
+    Increases the image contrast on a non-normalized image
+    :param image_list: in form of a list with values from 0 to 31
+    :return: image with a higher contrast
+    """
     image = Image.new('L', (IMAGE_SIZE, IMAGE_SIZE)) # L = 8bit grayscale
     image_list = [x * 255 / HIGHEST_PIXEL_VALUE for x in image_list]
     image.putdata(image_list)
@@ -28,7 +40,11 @@ def contrast(image_list):
     return image_list
 
 def rotate(image_list):
-    """Rotates the image with the eyes always pointing upwards"""
+    """
+    Rotates the image with the eyes always pointing upwards
+    :param image_list: in form of a list with values from 0 to 31
+    :return: rotated image with eyes pointing upwards
+    """
     darkest_pixels = _get_darkest_pixels(image_list)
     x, y = _calculate_mean_position(darkest_pixels)
     # move origin point to middle of image
@@ -46,6 +62,8 @@ def _get_darkest_pixels(image_list, dark_count=30):
     """
     Returns a list of the darkest pixel values in image
     List consits of tuples with a tuple of (pixel, position)
+    :param image_list: in form of a list with values from 0 to 31
+    :return: list of the <dark_count> darkest pixels
     """
 
     darkest_pixels = [] # consits of tutple of (pixel, position)
@@ -72,8 +90,10 @@ def _get_darkest_pixels(image_list, dark_count=30):
     return darkest_pixels
 
 def _calculate_mean_position(pixels):
-    """Calculates the mean position of all values in pixels"""
-    # pixels: (pixel, position)
+    """
+    Calculates the mean position of all values in pixels
+    :param pixels: list of pixels if form of tuples (pixel, position)
+    """
     mean_y = 0
     mean_x = 0
     for _, position in pixels:
